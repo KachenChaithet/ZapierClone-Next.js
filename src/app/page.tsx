aaf8a05b2aad1249e356b8f5e44a22ff3ec7a10e
Kachen Chaithet <chaytheskhchenthr@gmail.com>
2026-02-02 17:58:05 +0700

    03:trcp setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@/components/ui/button"
-import { prisma } from "@/lib/db"
+import ClientComponent from "./client";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const page = async () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+
   return (
     <div>
       <Button>Click me</Button>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>loading...</div>}>
+
+          <ClientComponent />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   )
 }
