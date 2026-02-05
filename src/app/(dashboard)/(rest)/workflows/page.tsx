3b0ea8303e33a9ab0456252f7c9e5fa09d917916
Kachen Chaithet <chaytheskhchenthr@gmail.com>
2026-02-05 19:58:22 +0700

    09: sidebar layout

---
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -0,0 +1,9 @@
+import { requireAuth } from "@/lib/auth-utils"
+
+const page = async () => {
+    await requireAuth()
+    return (
+        <div>this is workflow page</div>
+    )
+}
+export default page
